adcb973a20d41edb6b897c135fd31228b42f1384
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-30 10:12:26 +0530

    added loginapi

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-friends',
@@ -10,10 +11,27 @@ export class AddFriendsComponent {
   DescribeYourFriend =""
   friendNickName = ""
 
+  constructor(private api:ApiService){}
+  
+
   readValue = () =>
   {
     let data:any = {"name":this.name,"DescribeYourFriend":this.DescribeYourFriend,"friendNickName":this.friendNickName}
     console.log(data)
+    
+    this.api.addFriends(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status == "success") {
+          alert("Added Successfully")
+          this.name=""
+          this.friendNickName=""
+          this.DescribeYourFriend=""
+        } else {
+          alert("Something went wrong")
+        }
+      }
+    )
   }
 
 }
